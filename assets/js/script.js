1377ff335c61474b7f0affb34c47a9b2edb9dabe
alexmayberry <alexclimbs@gmail.com>
2022-07-27 09:33:21 -0700

    pseudo-coded major functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 $(document).ready( function() {console.log('Your Document has loaded!')});
+
+const apiKey = '5f7b77913d91556efcc7a0db5977a502'
+
+
+// Submit the form to fetch weather information
+
+// Handle button click to submit the form
+
+// Fetch geolocation information and pass to weather fetch function
+
+// Fetch weather information for geolocation
